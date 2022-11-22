77de5c38578233b30ddbbb4e7423061f86a90600
ElConan <105741929+ElConan@users.noreply.github.com>
2022-11-22 14:12:27 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ public class App {
         System.out.println("Hello, emilioale04!");
         System.out.println("Hello, Alejololer!");
         System.out.println("Hello, alejoal03!");
-        System.out.println("Hello, elconan!");
+        System.out.println("Hello, ElConan!");
         System.out.println("Hello, Jimmyuwu!");
     }
 }
